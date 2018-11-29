96729a51ddcc90e7bc29e32f0ecc493550a7b977
ROLLINGSTAAAAAAAAAAAAAAAAAAAAAAAAAAAART <45197550+ROLLINGSTAAAAAAAAAAAAAAAAAAAAAAAAAAAART@users.noreply.github.com>
2018-11-29 19:52:27 +0100

    Stuff

--- test_regressions.py
+++ test_regressions.py
@@ -4,108 +4,110 @@ Test des différents modèles linéaires
 Nécessite le wonderframe de formatage.py
 """
 from formatage import wonderframe
+from sklearn.metrics import mean_squared_error
+from math import sqrt
 
-## MODELE LINEAIRE
+## MODELE LINEAIRE 281
 from sklearn import linear_model
 regr=linear_model.LinearRegression()
 regr.fit(list(wonderframe['Historique'].values), list(wonderframe['Livraisons réelles'].values))
 predicto=regr.predict(list(wonderframe['Historique'].values))
-ecartmoy=sum(abs(list(wonderframe['Livraisons réelles'].values)-predicto))/len(predicto)
+ecartmoy=sqrt(mean_squared_error(wonderframe['Livraisons réelles'].values, predicto))#sum(abs(list(wonderframe['Livraisons réelles'].values)-predicto))/len(predicto)
 
-## MODELE LINEAIRE RIDGE ----> 93
+## MODELE LINEAIRE RIDGE ----> 146
 from sklearn import linear_model
 regr=linear_model.Ridge()
 regr.fit(list(wonderframe['Historique'].values), list(wonderframe['Livraisons réelles'].values))
 predicto=regr.predict(list(wonderframe['Historique'].values))
-ecartmoy=sum(abs(list(wonderframe['Livraisons réelles'].values)-predicto))/len(predicto)
+ecartmoy=sqrt(mean_squared_error(wonderframe['Livraisons réelles'].values, predicto))#ecartmoy=sum(abs(list(wonderframe['Livraisons réelles'].values)-predicto))/len(predicto)
 
-## MODELE LINEAIRE LASSO ----> 93
+## MODELE LINEAIRE LASSO ----> 146
 from sklearn import linear_model
 regr=linear_model.Lasso(max_iter = 1000000)
 regr.fit(list(wonderframe['Historique'].values), list(wonderframe['Livraisons réelles'].values))
 predicto=regr.predict(list(wonderframe['Historique'].values))
-ecartmoy=sum(abs(list(wonderframe['Livraisons réelles'].values)-predicto))/len(predicto)
+ecartmoy=sqrt(mean_squared_error(wonderframe['Livraisons réelles'].values, predicto))#ecartmoy=sum(abs(list(wonderframe['Livraisons réelles'].values)-predicto))/len(predicto)
 
-## MODELE LINEAIRE ELASTIC NET ----> 93
+## MODELE LINEAIRE ELASTIC NET ----> 146
 from sklearn import linear_model
 regr=linear_model.ElasticNet(max_iter = 1000000)
 regr.fit(list(wonderframe['Historique'].values), list(wonderframe['Livraisons réelles'].values))
 predicto=regr.predict(list(wonderframe['Historique'].values))
-ecartmoy=sum(abs(list(wonderframe['Livraisons réelles'].values)-predicto))/len(predicto)
+ecartmoy=sqrt(mean_squared_error(wonderframe['Livraisons réelles'].values, predicto))#ecartmoy=sum(abs(list(wonderframe['Livraisons réelles'].values)-predicto))/len(predicto)
 
-## MODELE LINEAIRE LARS ----> 115
+## MODELE LINEAIRE LARS ----> 165
 from sklearn import linear_model
 regr=linear_model.Lars()
 regr.fit(list(wonderframe['Historique'].values), list(wonderframe['Livraisons réelles'].values))
 predicto=regr.predict(list(wonderframe['Historique'].values))
-ecartmoy=sum(abs(list(wonderframe['Livraisons réelles'].values)-predicto))/len(predicto)
+ecartmoy=sqrt(mean_squared_error(wonderframe['Livraisons réelles'].values, predicto))#ecartmoy=sum(abs(list(wonderframe['Livraisons réelles'].values)-predicto))/len(predicto)
 
-## MODELE LINEAIRE ORTHOGONAL MATCHING PURSUIT ----> 183
+## MODELE LINEAIRE ORTHOGONAL MATCHING PURSUIT ----> 238
 from sklearn import linear_model
 regr=linear_model.OrthogonalMatchingPursuit()
 regr.fit(list(wonderframe['Historique'].values), list(wonderframe['Livraisons réelles'].values))
 predicto=regr.predict(list(wonderframe['Historique'].values))
-ecartmoy=sum(abs(list(wonderframe['Livraisons réelles'].values)-predicto))/len(predicto)
+ecartmoy=sqrt(mean_squared_error(wonderframe['Livraisons réelles'].values, predicto))#ecartmoy=sum(abs(list(wonderframe['Livraisons réelles'].values)-predicto))/len(predicto)
 
-## MODELE LINEAIRE BAYESIAN RIDGE ----> 194
+## MODELE LINEAIRE BAYESIAN RIDGE ----> 261
 from sklearn import linear_model
 regr=linear_model.BayesianRidge()
 regr.fit(list(wonderframe['Historique'].values), list(wonderframe['Livraisons réelles'].values))
 predicto=regr.predict(list(wonderframe['Historique'].values))
-ecartmoy=sum(abs(list(wonderframe['Livraisons réelles'].values)-predicto))/len(predicto)
+ecartmoy=sqrt(mean_squared_error(wonderframe['Livraisons réelles'].values, predicto))#ecartmoy=sum(abs(list(wonderframe['Livraisons réelles'].values)-predicto))/len(predicto)
 
-## MODELE LINEAIRE ARDREGRESSION ----> 180
+## MODELE LINEAIRE ARDREGRESSION ----> 226
 from sklearn import linear_model
 regr=linear_model.ARDRegression()
 regr.fit(list(wonderframe['Historique'].values), list(wonderframe['Livraisons réelles'].values))
 predicto=regr.predict(list(wonderframe['Historique'].values))
-ecartmoy=sum(abs(list(wonderframe['Livraisons réelles'].values)-predicto))/len(predicto)
+ecartmoy=sqrt(mean_squared_error(wonderframe['Livraisons réelles'].values, predicto))#ecartmoy=sum(abs(list(wonderframe['Livraisons réelles'].values)-predicto))/len(predicto)
 
-## MODELE LINEAIRE LOGISTIC REGRESSION ----> 0, 226 si pas tout
+## MODELE LINEAIRE LOGISTIC REGRESSION ----> 353
 from sklearn import linear_model
 regr=linear_model.LogisticRegression()
 regr.fit(list(wonderframe['Historique'][:15].values), list(wonderframe['Livraisons réelles'][:15].values))
 predicto=regr.predict(list(wonderframe['Historique'][15:].values))
-ecartmoy=sum(abs(list(wonderframe['Livraisons réelles'][15:].values)-predicto))/len(predicto)
+ecartmoy=sqrt(mean_squared_error(wonderframe['Livraisons réelles'][15:].values,predicto))
 
-## MODELE LINEAIRE ARDRegression ----> 180
+## MODELE LINEAIRE ARDRegression ----> 226
 from sklearn import linear_model
 regr=linear_model.ARDRegression()
 regr.fit(list(wonderframe['Historique'].values), list(wonderframe['Livraisons réelles'].values))
 predicto=regr.predict(list(wonderframe['Historique'].values))
-ecartmoy=sum(abs(list(wonderframe['Livraisons réelles'].values)-predicto))/len(predicto)
+ecartmoy=sqrt(mean_squared_error(wonderframe['Livraisons réelles'].values, predicto))#ecartmoy=sum(abs(list(wonderframe['Livraisons réelles'].values)-predicto))/len(predicto)
 
-## MODELE LINEAIRE SGDClassifier ----> 159
+## MODELE LINEAIRE SGDClassifier ----> 302
 from sklearn import linear_model
 regr=linear_model.SGDClassifier()
 regr.fit(list(wonderframe['Historique'].values), list(wonderframe['Livraisons réelles'].values))
 predicto=regr.predict(list(wonderframe['Historique'].values))
-ecartmoy=sum(abs(list(wonderframe['Livraisons réelles'].values)-predicto))/len(predicto)
+ecartmoy=sqrt(mean_squared_error(wonderframe['Livraisons réelles'].values, predicto))#ecartmoy=sum(abs(list(wonderframe['Livraisons réelles'].values)-predicto))/len(predicto)
 
 ## MODELE LINEAIRE SGDRegressor ----> 10^5
 from sklearn import linear_model
 regr=linear_model.SGDRegressor()
 regr.fit(list(wonderframe['Historique'].values), list(wonderframe['Livraisons réelles'].values))
 predicto=regr.predict(list(wonderframe['Historique'].values))
-ecartmoy=sum(abs(list(wonderframe['Livraisons réelles'].values)-predicto))/len(predicto)
+ecartmoy=sqrt(mean_squared_error(wonderframe['Livraisons réelles'].values, predicto))#ecartmoy=sum(abs(list(wonderframe['Livraisons réelles'].values)-predicto))/len(predicto)
 
-## MODELE LINEAIRE Perceptron ----> 171
+## MODELE LINEAIRE Perceptron ----> 307
 from sklearn import linear_model
 regr=linear_model.Perceptron()
 regr.fit(list(wonderframe['Historique'].values), list(wonderframe['Livraisons réelles'].values))
 predicto=regr.predict(list(wonderframe['Historique'].values))
-ecartmoy=sum(abs(list(wonderframe['Livraisons réelles'].values)-predicto))/len(predicto)
+ecartmoy=sqrt(mean_squared_error(wonderframe['Livraisons réelles'].values, predicto))#ecartmoy=sum(abs(list(wonderframe['Livraisons réelles'].values)-predicto))/len(predicto)
 
-## MODELE LINEAIRE PassiveAggressiveClassifier ----> 105
+## MODELE LINEAIRE PassiveAggressiveClassifier ----> 260
 from sklearn import linear_model
 regr=linear_model.PassiveAggressiveClassifier()
 regr.fit(list(wonderframe['Historique'].values), list(wonderframe['Livraisons réelles'].values))
 predicto=regr.predict(list(wonderframe['Historique'].values))
-ecartmoy=sum(abs(list(wonderframe['Livraisons réelles'].values)-predicto))/len(predicto)
+ecartmoy=sqrt(mean_squared_error(wonderframe['Livraisons réelles'].values, predicto))#ecartmoy=sum(abs(list(wonderframe['Livraisons réelles'].values)-predicto))/len(predicto)
 
 ## MODELE LINEAIRE RANSACRegressor ----> ?
 from sklearn import linear_model
 regr=linear_model.RANSACRegressor()
 regr.fit(list(wonderframe['Historique'].values), list(wonderframe['Livraisons réelles'].values))
 predicto=regr.predict(list(wonderframe['Historique'].values))
-ecartmoy=sum(abs(list(wonderframe['Livraisons réelles'].values)-predicto))/len(predicto)
+ecartmoy=sqrt(mean_squared_error(wonderframe['Livraisons réelles'].values, predicto))#ecartmoy=sum(abs(list(wonderframe['Livraisons réelles'].values)-predicto))/len(predicto)
--- timeseries.py
+++ timeseries.py
@@ -6,17 +6,17 @@ from statsmodels.tsa.holtwinters import SimpleExpSmoothing
 import math
 import numpy as np
 from formatage import wonderframe
+from sklearn.metrics import mean_squared_error
 
 turfu=[]
 for i in range(10,len(wonderframe)):
-    print(i)
     fit=SimpleExpSmoothing(wonderframe['Historique'][i][-i-1:]).fit(smoothing_level=0.6,optimized=True)
     turfu.append(fit.forecast(1)[0])
 
 diff=0
 for i in range(10,len(turfu)):
     diff+=(wonderframe["Livraisons réelles"][10+i]-turfu[i])**2
-diff=math.sqrt(diff)
+diff=math.sqrt(diff/len(turfu))
 
 turfuDeBase=[]
 for i in range(len(wonderframe)):
@@ -26,4 +26,15 @@ for i in range(len(wonderframe)):
 diffDeBase=0
 for i in range(len(turfuDeBase)):
     diffDeBase+=(wonderframe["Livraisons réelles"][i]-turfuDeBase[i])**2
-diffDeBase=math.sqrt(diff)
+diffDeBase=math.sqrt(diffDeBase/len(turfuDeBase))
+diffDeBase=math.sqrt(mean_squared_error(wonderframe['Livraisons réelles'].values,turfuDeBase))
+
+turfuLourd=[]
+for i in range(len(wonderframe)):
+    a=np.average(wonderframe['Historique'][i][-i-1:],weights=[i+1 for i in range(i+1)])
+    turfuLourd.append(a)
+
+diffLourd=0
+for i in range(len(turfuLourd)):
+    diffLourd+=(wonderframe["Livraisons réelles"][i]-turfuLourd[i])**2
+diffLourd=math.sqrt(diffLourd/len(turfuLourd))
