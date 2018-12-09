3610258b4a5ab76bbb54fde605d08bbe836449c9
ROLLINGSTAAAAAAAAAAAAAAAAAAAAAAAAAAAART <45197550+ROLLINGSTAAAAAAAAAAAAAAAAAAAAAAAAAAAART@users.noreply.github.com>
2018-12-09 17:40:57 +0100

    Add files via upload

--- interfaceweb.py
+++ interfaceweb.py
@@ -15,6 +15,75 @@ from werkzeug import secure_filename
 
 app = Flask(__name__)
 
+import pandas as pd
+import numpy as np
+from sklearn.metrics import mean_squared_error
+from sklearn import linear_model
+from math import sqrt
+from statsmodels.tsa.api import ExponentialSmoothing
+
+@app.route('/comparaison')
+def cmp():
+    datas=formaterXLSX()
+    histo=list(datas['Historique'].values)
+    livraisons=list(datas['Livraisons réelles'].values)
+    semaines=list(datas['Semaines'].values)
+
+    
+    predict=[]
+    for i in range(len(histo)):
+        predict.append(np.mean(histo[i][-i-1:]))
+    errquadmoy=round(sqrt(mean_squared_error(predict,livraisons)),2)
+    
+    regr=linear_model.Ridge()
+    regr.fit(histo, livraisons)
+    predictregr=regr.predict(histo)
+    ecartmoyregr=round(sqrt(mean_squared_error(livraisons, predictregr)),2)    
+    
+    #Exponential Moving Average
+    ema=[]
+    for i in range(len(histo)):
+        ema.append(pd.DataFrame(histo[i]).ewm(alpha=0.045,adjust=False).mean())
+    predictewma=[]
+    for i in range(len(histo)):
+        predictewma.append(ema[i].iloc[35,0])
+    errquadmewma=round(sqrt(mean_squared_error(predictewma,livraisons)),2)
+    
+    
+    
+    #Holter's Winter
+    listlivraison=livraisons
+    
+    modelholter = ExponentialSmoothing(listlivraison,seasonal_periods=35,trend='add',seasonal='add').fit()
+    predictwinter = modelholter.forecast(len(listlivraison))
+
+    img = BytesIO() 
+    plt.figure()
+
+    r= list(range(1,len(semaines)+1))
+    for i in range(len(semaines)):
+            for j in range(len(histo[i])):
+               plt.scatter(r[i],histo[i][j],s=2, c="green")           
+    plt.plot(r,livraisons,label="Livraisons réelles")
+    plt.plot(r,predict,label="Moyenne")
+    plt.plot(r,predictewma,label="Exponential moving average")
+    plt.plot(r,predictregr,label="Régression linéaire")
+    plt.ylabel("Quantités")
+    plt.xlabel('Semaines')
+    plt.title("Comparaison des algorithmes")
+    plt.legend()
+
+    
+    plt.savefig(img, format='png')
+    img.seek(0)
+    
+    plot_data = quote(base64.b64encode(img.read()).decode())
+    
+    livraisons=np.insert(livraisons,35,livraisons[35])
+    predictwinter=np.insert(predictwinter,0,predictwinter[0]) 
+    errquadwinter=round(sqrt(mean_squared_error(predictwinter,livraisons)),2)   
+    return render_template("comparaison.html",plot_url=plot_data,err1=errquadmoy,err2=ecartmoyregr,err3=errquadmewma,err4=errquadwinter)
+
 @app.route('/miniup', methods = ['GET', 'POST'])
 def mu():
     if request.method == 'POST':
